eea391ee4ffc888ce23621678f614d21f1e4b646
Vincent Toon <vincenttoon22@gmail.com>
2024-01-04 22:00:50 -0600

    script connected

---
+++ script.js
@@ -0,0 +1,12 @@
+let images = [
+  "dice-1.jpg",
+  "dice-2.jpg",
+  "dice-3.jpg",
+  "dice-4.jpg",
+  "dice-5.jpg",
+  "dice-6.jpg",
+];
+
+let dice = document.querySelectorAll("img");
+
+console.log(dice);
