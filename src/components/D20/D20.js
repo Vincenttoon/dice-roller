35dc86ea1fba4cc7cd54ec743acdaf383d08723f
Vincent Toon <vincenttoon22@gmail.com>
2024-01-10 13:16:07 -0600

    d20 functioning and looking good

--- src/components/D20/D20.js
+++ src/components/D20/D20.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 import "../D20/D20.scss";
 
@@ -47,40 +47,78 @@ const D20 = () => {
     D20Dice20,
   ];
 
-  const [dieSrc, setDieSrc] = useState(images[0]);
+  const [dieSrc1, setDieSrc1] = useState(images[0]);
+  const [dieSrc2, setDieSrc2] = useState(images[0]);
   const [isRolling, setIsRolling] = useState(false);
+  const [rollTwo, setRollTwo] = useState(false);
+  const [rollResult, setRollResult] = useState("");
 
-  const roll = () => {
+  const timeoutRef = useRef(null);
+
+  const rollDice = () => {
     setIsRolling(true);
-    setTimeout(() => {
+    timeoutRef.current = setTimeout(() => {
       setIsRolling(false);
-      const dieValue = Math.floor(Math.random() * 20);
 
-      setDieSrc(images[dieValue]);
+      const dieValue1 = Math.floor(Math.random() * 20);
+      setDieSrc1(images[dieValue1]);
 
-      document.querySelector("#total").innerHTML =
-        "Your roll is " + (dieValue + 1);
+      if (rollTwo) {
+        const dieValue2 = Math.floor(Math.random() * 20);
+        setDieSrc2(images[dieValue2]);
+
+        const higherRoll = Math.max(dieValue1 + 1, dieValue2 + 1);
+        const combinedTotal = dieValue1 + 1 + dieValue2 + 1;
+
+        setRollResult(
+          `Higher roll is ${higherRoll}. Combined total is ${combinedTotal}`
+        );
+      } else {
+        setRollResult(`Your roll is ${dieValue1 + 1}`);
+      }
     }, 1000);
-    document.querySelector("#total").style.fontSize = "1.5rem";
   };
 
   useEffect(() => {
-    roll();
+    rollDice();
+    return () => clearTimeout(timeoutRef.current);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [rollTwo]);
 
   return (
     <div className="container">
       <div className="dice-wrapper">
         <img
-          src={dieSrc}
+          src={dieSrc1}
           className={isRolling ? "dice shake" : "dice"}
           alt="D20"
         />
+        {rollTwo && (
+          <img
+            src={dieSrc2}
+            className={isRolling ? "dice shake" : "dice"}
+            alt="D20"
+          />
+        )}
       </div>
-      <p id="total"></p>
-      <button onClick={roll} disabled={isRolling}>
-        ROLL
+      <p className="resultText">{rollResult}</p>
+      <button
+        onClick={() => {
+          setRollTwo(false);
+          rollDice();
+        }}
+        disabled={isRolling}
+      >
+        ROLL ONE
+      </button>
+      <button
+        onClick={() => {
+          setRollTwo(true);
+          rollDice();
+        }}
+        disabled={isRolling}
+      >
+        ROLL TWO
       </button>
     </div>
   );
