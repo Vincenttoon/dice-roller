0624ec291f7cb18d2d69f99d5c28f0184e709908
Vincent Toon <vincenttoon22@gmail.com>
2024-01-10 15:06:23 -0600

    footer added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header/Header";
+import Footer from './Footer/Footer';
 
 import classes from "./Layout.module.scss";
 
@@ -8,6 +9,7 @@ const Layout = ({ children }) => {
     <>
       <Header />
       <div className={classes.container}>{children}</div>
+      <Footer />
     </>
   );
 };
